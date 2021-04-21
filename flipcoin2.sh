a185ca05081a23c25a075c5d926195f2f6ac1c60
shrutiP1 <shrutipatil13798@gmail.com>
2021-04-22 01:54:37 +0530

    calculating percentage of singlet combination

--- flipcoin2.sh
+++ flipcoin2.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 echo "welcome"
 
 #UC1
@@ -11,4 +12,28 @@ then
 else
    echo "tail"
 fi
+
+#UC2
+read -p "how many time you want to loop it " num
+headcount=0;
+tailcount=0;
+for (( i=0 ; i<$num ; i++ ))
+do
+     res=$((RANDOM%2))
+
+   if [ $res -eq 1 ]
+   then
+      ((headcount++))
+   else
+      ((tailcount++))
+   fi
+done
+
+declare -A countDic
+countDic[head]=$headcount
+countDic[tail]=$tailcount
+perOfHead=$(((${countDic[head]})*(100/$num)))
+echo "head percentage "$perOfHead "%"
+perOfTail=$(((${countDic[tail]})*(100/$num)))
+echo "tail percentage "$perOfTail "%"
 
